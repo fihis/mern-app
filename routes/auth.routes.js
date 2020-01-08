3973ac877f2747e48ab4da832d111f5ddbf48d61
fihis <isifuo@gmail.com>
2020-01-08 07:32:08 +0300

    feat: add registering

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -17,6 +17,7 @@ router.post(
     async (req, res) => {
         try {
             const errors = validationResult(req);
+
             if (!errors.isEmpty()) {
                 return res.status(400).json({
                     errors: errors.array(),
